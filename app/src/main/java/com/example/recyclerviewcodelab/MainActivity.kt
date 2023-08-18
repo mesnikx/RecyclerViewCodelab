cff1a560faf8ee6f42514655e419f97bc9d7fa12
Michael Rudakov <mishelrudakov@yandex.ru>
2023-08-18 10:32:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/recyclerviewcodelab/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.recyclerviewcodelab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewcodelab.adapter.ItemAdapter
+import com.example.recyclerviewcodelab.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
+    }
+}
---
+++ app/src/main/java/com/example/recyclerviewcodelab/model/Affirmation.kt
@@ -0,0 +1,3 @@
+package com.example.recyclerviewcodelab.model
+
+data class Affirmation(val stringResourseId: Int)
